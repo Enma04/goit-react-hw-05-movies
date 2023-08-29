25bc73828ffc4d33557772a0067be318efef077b
Enma04 <118761090+Enma04@users.noreply.github.com>
2023-08-30 01:33:38 +1000

    cast completado

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,7 +1,36 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getMovie } from "resources/moviesData";
+import css from './Cast.module.css';
+
+const imageBaseURL = 'https://image.tmdb.org/t/p/w500';
+
 export const Cast = () => {
+	const { id } = useParams();
+	const [cast, setCast] = useState([]);
+
+  useEffect(() => {
+    async function newCast() {
+      const data = await getMovie(id, '/credits');
+      setCast(data.cast);
+    }
+    newCast();
+  }, [id]);
+
 	return(
-		<div>
-			<span>Cast</span>
-		</div>
+		<ul className={css.list}>
+			{
+				cast.map((person) => (
+					<li key={person.id} className={css.card}>
+						<img src={`${imageBaseURL}${person.profile_path}`}
+								alt="Imagen"
+								className={css.imagen}
+						/>
+						<span className={css.text}> Name: {person.name} </span>
+						<span className={css.text}> Character: {person.character} </span>
+					</li>
+				))
+			}
+		</ul>
 	);
 }
--- src/resources/moviesData.js
+++ src/resources/moviesData.js
@@ -20,8 +20,8 @@ export const allMovies = () => {
     });
 }
 
-export const getMovie = (id) => {
-  return fetch(`${url}${id}?api_key=${myKeyTMDB}`).then(response => {
+export const getMovie = (id, cast='') => {
+  return fetch(`${url}${id}${cast}?api_key=${myKeyTMDB}`).then(response => {
     if(!response.ok) {
       throw new Error(response.status); 
     }
